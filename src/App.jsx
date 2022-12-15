87bce549a06212a221c473490e6c806e854e529d
audrey <mbozorgi604@gmail.com>
2022-12-15 22:48:46 +0330

    creating our first react component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            hello world im here
+        </div>
+    )
+}
+
+export default App
+
+//*** this is first react component 
